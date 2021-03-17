9780d485685fa6ead114cb524ddc65e14358577f
Andrey Simonov <sasmeo@gmail.com>
2021-03-17 23:49:01 +0800

    fix3

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,37 +2,29 @@ import cn from 'classnames';
 import s from './style.module.css';
 
 const Menu = ({isActive, onChangeMenuActive}) => {
-  return (
-    <>
-        <div className={cn(s.menuContainer, {[s.active]: isActive}, {[s.deactive]: !isActive})}>
-            <div className={s.overlay} />
-                <div className={s.menuItems}>
-                    <ul>
-                        <li>
-                            <a href="#welcome">
-                            HOME
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#game">
-                            GAME
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#about">
-                            ABOUT
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#contact">
-                            CONTACT
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-        </div>
-    </>
-  );
+    const menuList = ['welcome', 'game', 'about', 'contact'];
+    const menuItems = menuList.map((name) => {
+        return (
+            <li>
+                <a href={"#"+name}>
+                    {name.toUpperCase()}
+                </a>
+            </li>
+        )
+        }
+    );
+    return (
+        <>
+            <div className={cn(s.menuContainer, {[s.active]: isActive, [s.deactive]: !isActive})}>
+                <div className={s.overlay} />
+                    <div className={s.menuItems}>
+                        <ul>
+                            {menuItems}
+                        </ul>
+                    </div>
+            </div>
+        </>
+    );
 }
 
 export default Menu;
--- src/components/MenuHeader/index.js
+++ src/components/MenuHeader/index.js
@@ -4,7 +4,7 @@ import NavBar from '../NavBar';
 
 const MenuHeader = () => {
   const [menuActive, setMenuActive] = useState(false);
-  const handleChangeActive = (menuActive) => {
+  const handleChangeActive = () => {
     setMenuActive(!menuActive);
   }
   return (
