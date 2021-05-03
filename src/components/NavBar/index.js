5418273c237959972c7d1d0891935cdc714e1032
Andrey Simonov <sasmeo@gmail.com>
2021-05-03 09:28:45 +0800

    fix3

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,19 +1,27 @@
 import cn from 'classnames';
 import s from './style.module.css';
 
-const NavBar = ({isOpen, bgActive = false, onClickHamburg}) => {
+import {ReactComponent as LoginSVG} from '../../assets/login.svg';
+
+const NavBar = ({isOpen, bgActive = false, onClickHamburg, onClickLogin}) => {
   return (
     <nav id={s.navbar} className={cn({
         [s.bgActive]: bgActive
     })}>
         <div className={s.navWrapper}>
-            <p className={s.brand}>
+            <div className={s.brand}>
                 LOGO
-            </p>
-            <div className={cn(s.menuButton, {
-                [s.active]: isOpen === true
-            })} onClick={onClickHamburg} >
-                <span />
+            </div>
+            <div className={s.loginAndMenu}>
+                <div className={s.loginWrap}
+                    onClick={onClickLogin}>
+                    <LoginSVG />
+                </div>
+                <div className={cn(s.menuButton, {
+                    [s.active]: isOpen === true
+                })} onClick={onClickHamburg} >
+                    <span />
+                </div>
             </div>
         </div>
     </nav>
